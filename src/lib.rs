50c5682a2cab317667b2841f48eefdc2d3b9561b
Markus Ast <m@rkusa.st>
2017-04-07 09:55:35 +0200

    fix package level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-///! Ctx defines the Context type, which carries deadlines, cancelation
-///! [futures](https://github.com/alexcrichton/futures-rs), and other request-scoped values across API
-///! boundaries and between processes.
-///!
-///! It is similar to Go's [context](https://blog.golang.org/context)
-///! [package](https://golang.org/pkg/context/). The main use case is to have incoming requests to a
-///! server create a Context. This Context is propagated in the chain of function calls between the
-///! incoming request until the outging response. On its way, the Context can be replaced with a
-///! derived Context using `with_cancel`, `with_deadline`, `with_timeout`, or `with_value`.
+//! Ctx defines the Context type, which carries deadlines, cancelation
+//! [futures](https://github.com/alexcrichton/futures-rs), and other request-scoped values across API
+//! boundaries and between processes.
+//!
+//! It is similar to Go's [context](https://blog.golang.org/context)
+//! [package](https://golang.org/pkg/context/). The main use case is to have incoming requests to a
+//! server create a Context. This Context is propagated in the chain of function calls between the
+//! incoming request until the outging response. On its way, the Context can be replaced with a
+//! derived Context using `with_cancel`, `with_deadline`, `with_timeout`, or `with_value`.
 
 extern crate futures;
 extern crate tokio_timer;
